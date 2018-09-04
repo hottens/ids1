a9d597c6cedc073c41d66e185ac2eea892f69f95
Unknown <hayo.ottens@gmail.com>
2018-09-04 14:44:25 +0200

    complete assambly of information

--- assignment1.py
+++ assignment1.py
@@ -17,36 +17,48 @@ def valid(row):
 
 with open("movievalue.csv") as f:
     reader = csv.DictReader(f)
-    data = []
+    data = {}
     for row in reader:
-        if(valid(row)): data.append(row)
+        if(valid(row)):
+		data[row["Title"]] = {}
+		data[row["Title"]]["CSV"] = row
 
-def getURL(title, year):
+def getOMDBURL(title, year):
+	return "http://www.omdbapi.com/?t="+title+"&y="+year+"&apikey=e121dcf3" 
+
+def getTMDBURL(title, year):
 	return "https://api.themoviedb.org/3/search/movie?api_key=3ce50e41bbff335a1a1a7a054f2b141b&language=en-US&query="+title+"&page=1&include_adult=false&primary_release_year="+year
 
 genreById = {}
 def getGenres():
-	genreURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=3ce50e41bbff335a1a1a7a054f2b141b&language=en-US"
-	genresResult = urllib.urlopen(genreURL)
-	genres = json.loads(genresResult.read())
+	genreTMDBURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=3ce50e41bbff335a1a1a7a054f2b141b&language=en-US"
+	genresTMDBResult = urllib.urlopen(genreTMDBURL)
+	genres = json.loads(genresTMDBResult.read())["genres"]
 
 	for genre in genres:
 		genreById[genre["id"]] = genre["name"]
 getGenres()
 x = 0
-for d in data:
-	title = d["Title"]
-	day,month,year = d["ReleaseDate"].split("/")
-	response = urllib.urlopen(getURL(title,year))
-	print getURL(title,year)
-	result = json.loads(response.read())
-# Genre, imdbRating, imdbVotes (and optional also Director, Country, PG rating, etc.)
-	for r in result["results"]:
-		if r["title"]==title or r["original_title"]==title: #print r
-			d["genres"] = []
+for title, values in data.items():
+	csv = values["CSV"]
+
+	# Get from TMDB
+	day,month,year = csv["ReleaseDate"].split("/")
+	responseTMDB = urllib.urlopen(getTMDBURL(title,year))
+	resultTMDB = json.loads(responseTMDB.read())
+	for r in resultTMDB["results"]:
+		if r["title"]==title or r["original_title"]==title: 
+			# Add genres			
+			r["genres"] = []
 			for g in r["genre_ids"]:
-				d["genres"].append(genreById[g])
-	print d
-			
-	x+=1
-	if x>3: break
+				r["genres"].append(genreById[g])
+			values["TMDB"] = r
+
+	# Get from OMDB
+	responseOMDB = urllib.urlopen(getOMDBURL(title,year))
+	resultOMDB = json.loads(responseOMDB.read())
+	values["OMDB"] = resultOMDB
+
+# Save extended dict
+with open('moviedata.txt', 'w') as file:
+     file.write(json.dumps(data)) # use `json.loads` to do the reverse
